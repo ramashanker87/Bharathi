18ddc81e42a833373ee5fadf28f347068a03c9ea
admin <rama@gail.com>
2025-02-16 19:10:50 -0800

    Day17

---
+++ Day17/student-management-app/rabbitmq-student-producer/src/main/java/app/bharathi/com/rabbitmq_student_producer/RabbitmqStudentProducerApplication.java
@@ -0,0 +1,13 @@
+package app.bharathi.com.rabbitmq_student_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqStudentProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqStudentProducerApplication.class, args);
+	}
+
+}
