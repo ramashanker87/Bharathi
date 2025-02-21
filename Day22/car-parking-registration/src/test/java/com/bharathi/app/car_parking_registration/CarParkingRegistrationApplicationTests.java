639a7de84caabfa4f375f557286d497588a57959
Bharathi <bharathim@gmail.com>
2025-02-21 14:20:16 +0530

    updated the files

---
+++ Day22/car-parking-registration/src/test/java/com/bharathi/app/car_parking_registration/CarParkingRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharathi.app.car_parking_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarParkingRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
