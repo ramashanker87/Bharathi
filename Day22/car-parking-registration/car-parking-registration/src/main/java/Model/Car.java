bdd839ed8d4a8e4b6cfbacb7b0efeee538c497f8
admin <rama@gail.com>
2025-03-02 22:32:53 -0800

    Day22 new commit

---
+++ Day22/car-parking-registration/car-parking-registration/src/main/java/Model/Car.java
@@ -0,0 +1,10 @@
+package Model;
+
+public class Car {
+    private String ownerName;
+    private String regNo;
+    private String module;
+    private String type; // Fuel type (e.g., Petrol, Diesel, Electric)
+
+    // Getters and Setters
+}
