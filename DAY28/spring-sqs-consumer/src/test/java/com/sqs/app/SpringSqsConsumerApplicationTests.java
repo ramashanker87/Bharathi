da734d38f76dd41a0ff3433a764a55cfb322e27d
admin <rama@gail.com>
2025-03-06 07:26:22 -0800

    new commit Day28

---
+++ DAY28/spring-sqs-consumer/src/test/java/com/sqs/app/SpringSqsConsumerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sqs.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSqsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+.
